ad9105faabae7b86e74cf9cb662ba5bc1776ca82
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-26 15:05:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const body = document.body;
+	const para = document.createElement("p");
+	para.innerText="DOM load success";
+	body.appendChild(para)
+	
+})
